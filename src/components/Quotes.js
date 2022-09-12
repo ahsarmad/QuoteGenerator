9b3810946fffdb32c6861bce1b84635fb0111e52
Sarmad <afzalsarmad77@gmail.com>
2022-09-12 14:03:26 -0400

    Initialize project repo

---
+++ src/components/Quotes.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Quotes = () => {
+  return (
+    <div>
+      <blockquote>This is a great quote!</blockquote>
+      <p>-Your name</p>
+    </div>
+  );
+};
+
+export default Quotes;
